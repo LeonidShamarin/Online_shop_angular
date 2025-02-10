6969cf8d77db3c50b155d891a011a2e4c2cf3310
Leonid Shamarin <Leonideko1@gmail.com>
2025-02-10 14:20:58 +0200

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { CommonModule } from '@angular/common';
+import { AppComponent } from './app.component';
+import { ProductComponent } from './components/product/product.component';
+import { HttpClientModule } from '@angular/common/http';
+import { GlobalErrorComponent } from './components/global-error/global-error.component';
+import { FilterProductsPipe } from './pipes/filter-products.pipe';
+import { ModalComponent } from './components/modal/modal.component';
+import { CreateProductComponent } from './components/create-product/create-product.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FocusDirective } from './directives/focus.directive';
+import { ProductPageComponent } from './pages/product-page/product-page.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductComponent,
+    GlobalErrorComponent,
+    FilterProductsPipe,
+    ModalComponent,
+    CreateProductComponent,
+    FocusDirective,
+    ProductPageComponent,
+    AboutPageComponent,
+    NavigationComponent,
+  ],
+  imports: [
+    BrowserModule,
+    CommonModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ src/app/pages/about-page/about-page.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit  } from '@angular/core';
+
+@Component({
+  selector: 'app-about-page',
+  templateUrl: './about-page.component.html',
+  styleUrl: './about-page.component.scss'
+})
+export class AboutPageComponent implements OnInit {
+  ngOnInit(): void {
+  }
+}
